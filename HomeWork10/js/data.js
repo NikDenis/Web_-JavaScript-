0932174fadebdda3b52ee06341f8116b9ff95db6
Denis <den24.03@mail.ru>
2023-04-19 22:23:00 +0500

    Добавил homework10

---
+++ HomeWork10/js/data.js
@@ -0,0 +1,31 @@
+// Задание 1
+// 1. Поиск в интернете (бесплатные api примеры).
+// 2. Найти любые данные, на произвольную тему.
+// 3. Создать файл data.js.
+// 4. Создать переменную которая будет хранить данные из публичных api.
+
+// Придумал свое)
+
+export const dataCars =  `[
+    {
+        "id": "1",
+        "brand": "BMW",
+        "year": "2000",
+        "model": "320i",
+        "url": "https://www.carfarm.com.hk/wp-content/uploads/2021/05/DSC03490-300x300.jpg"
+    },
+    {
+        "id": "1",
+        "brand": "GAZ",
+        "year": "1960",
+        "model": "21-ус",
+        "url": "https://p.turbosquid.com/ts-thumb/H1/AHQPqC/HxQ5G0kT/gaz21volga1962417983126/jpg/1487998596/300x300/sharp_fit_q85/ee75a451035c6331f668530241e25c8aea6de286/gaz21volga1962417983126.jpg"
+    },
+    {
+        "id": "1",
+        "brand": "VAZ",
+        "year": "1990",
+        "model": "21099",
+        "url": "https://www.meme-arsenal.com/memes/3e04f68e0e7bc80b34534add245352ee.jpg"
+    }
+]`;
