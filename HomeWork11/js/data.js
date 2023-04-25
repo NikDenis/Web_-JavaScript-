f3246a8823a609a20fa802b408416575836e5b42
Denis <den24.03@mail.ru>
2023-04-25 16:32:41 +0500

    добавил HomeWork11

---
+++ HomeWork11/js/data.js
@@ -0,0 +1,42 @@
+
+
+export const dataProducts = `[
+  {
+  "image": "img/products-cart1.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  },
+  {
+  "image": "img/products-cart2.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  },
+  {
+  "image": "img/products-cart3.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  },
+  {
+  "image": "img/products-cart4.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  },
+  {
+  "image": "img/products-cart5.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  },
+  {
+  "image": "img/products-cart6.jpg",
+  "name": "Ellery x&nbsp;m&rsquo;o capsuleEllery x&nbsp;m&rsquo;o capsule",
+  "discription": "Known for her sculptural takes on&nbsp;traditional tailoring,Australian arbiter of&nbsp;cool Kym Ellery teams up&nbsp;with Moda Operandi.",
+  "price": 52
+  }
+]`;
+
+
